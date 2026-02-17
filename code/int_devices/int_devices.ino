ce31a2ca25bef65f1a5440278b9fb5af9b67e3cc
Jack Cains <jackcains101@gmail.com>
2026-02-17 13:32:54 +0000

    fully functioning arduino code to send string down the serial port. need to change python code to handle asetniop inputs.

--- code/int_devices/int_devices.ino
+++ code/int_devices/int_devices.ino
@@ -1,7 +1,13 @@
 const int pins[] = {36, 35, 34, 33, 29, 30, 31, 32}; // Pins in use
 const char* labels[] = {"A", "S", "E", "T", "N", "I", "O", "P"}; // ASETNIOP keys
-const int pinCount = 8; // Keyboard size
+bool pressed[] = {false, false, false, false, false, false, false, false};
+const int pinCount = 8; // Keyboard size'
 
+int lastState[] = {0,0,0,0,0,0,0,0};
+bool send = false;
+bool stateChange = false;
+
+char input[20];
 
 
 void setup() {
@@ -16,16 +22,44 @@ void setup() {
   }
 
   Serial.println("Board initialised...");
+  strcpy(input,"");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   for (int i = 0; i < pinCount; i++) {
-    if (digitalRead(pins[i]) == HIGH) {
-      Serial.println(labels[i]);
+    int buttonState = digitalRead(pins[i]);
+    if (buttonState == HIGH && !pressed[i]) {
+      strcat(input,labels[i]);
+      pressed[i] = true;
       digitalWrite(LED_BUILTIN, HIGH);
     }
+    if (buttonState != lastState[i]){
+      stateChange = true;
+    }
+    lastState[i] = buttonState;
+  }
+
+  if(stateChange){
+    stateChange = false;
+    for (int i = 0; i < pinCount; i++) {
+      int buttonState = digitalRead(pins[i]);
+      if(buttonState == HIGH){
+        send = false;
+        break;
+      }
+      send = true;
+    }
+  }
+
+  if (send) { // released
+    Serial.println(input);
+    strcpy(input,"");
+    for (int p = 0; p < 8; p++) {
+      pressed[p] = false;
+    }
+    send = false;
   }
   delay(250);
   digitalWrite(LED_BUILTIN, LOW);
