b1b9dc9c78de4afa71af0cb0086a2d96c8fa17c8
Jack Jarvis <jackjarvis1123@gmail.com>
2026-02-17 11:43:54 +0000

    Data collection code

---
+++ ResistiveTouch/ResistiveTouch.ino
@@ -0,0 +1,41 @@
+const int ROWS = 5;
+const int COLS = 4;
+
+const int ROW_PINS[ROWS] = {1, 2, 3, 4, 9}; 
+const int COL_PINS[COLS] = {5, 6, 7, 8};
+
+void setup() {
+  Serial.begin(115200);
+  
+  for (int i = 0; i < ROWS; i++) {
+    pinMode(ROW_PINS[i], OUTPUT);
+    digitalWrite(ROW_PINS[i], LOW);
+  }
+
+  for (int i = 0; i < COLS; i++) {
+    pinMode(COL_PINS[i], INPUT);
+  }
+
+  analogReadResolution(12);
+}
+
+void loop() {
+  for (int rowIndex = 0; rowIndex < ROWS; rowIndex++) {
+    digitalWrite(ROW_PINS[rowIndex], HIGH);
+    delayMicroseconds(50); 
+
+    for (int colIndex = 0; colIndex < COLS; colIndex++) {
+      int val = analogRead(COL_PINS[colIndex]);
+      Serial.print(val);
+      
+      if (!(rowIndex == ROWS - 1 && colIndex == COLS - 1)) {
+        Serial.print(",");
+      }
+    }
+    digitalWrite(ROW_PINS[rowIndex], LOW);
+  }
+  
+  Serial.println();
+  
+  delay(100);
+}
